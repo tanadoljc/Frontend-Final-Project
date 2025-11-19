2ffb3190d5bb220fa445bb5fdb441baa94f50a15
Tanadol Jaichuen <tanadol.j@houseofdev.tech>
2025-11-19 15:12:31 +0700

    fix: middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,15 @@
-import { withAuth } from 'next-auth/middleware';
+import { getToken } from 'next-auth/jwt';
+import { NextResponse } from 'next/server';
 
-// ต้อง export default function (หรือ export function middleware)
-export default withAuth({
-  callbacks: {
-    authorized: ({ token }) => !!token, // บังคับให้ต้อง login
-  },
-});
+export async function middleware(req: any) {
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+
+  if (!token) {
+    return NextResponse.redirect(new URL('/api/auth/signin', req.url));
+  }
+
+  return NextResponse.next();
+}
 
 export const config = {
   matcher: [
