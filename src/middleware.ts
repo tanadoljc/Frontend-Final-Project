8900916b01477f8d3f5a88d09252aed205481162
Tanadol Jaichuen <tanadol.j@houseofdev.tech>
2025-11-19 11:18:57 +0700

    fix: middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,18 @@
-export { default } from 'next-auth/middleware';
+import { withAuth } from 'next-auth/middleware';
+
+// ต้อง export default function (หรือ export function middleware)
+export default withAuth({
+  callbacks: {
+    authorized: ({ token }) => !!token, // บังคับให้ต้อง login
+  },
+});
 
 export const config = {
   matcher: [
-    '/mybooking/:path*', 
+    '/mybooking/:path*',
     '/profile/:path*',
     '/exhibition/create',
     '/exhibition/create-booking/:path*',
     '/exhibition/edit/:path*',
   ],
-}
+};
