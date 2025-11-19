e526f65a23e17e3d76c24d1acd8653dc9331b7ae
Tanadol Jaichuen <tanadol.j@houseofdev.tech>
2025-11-19 10:52:25 +0700

    fix: userLogin import

--- src/app/api/auth/[...nextauth]/authOptions.ts
+++ src/app/api/auth/[...nextauth]/authOptions.ts
@@ -1,39 +1,42 @@
-import userLogIn from "@/libs/userLogIn";
-import { AuthOptions } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
+import userLogIn from '@/libs/userLogin';
+import { AuthOptions } from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
 
 export const authOptions: AuthOptions = {
   providers: [
     // Authentication provider, use Credentials Provider
     CredentialsProvider({
       // The name to display on the sign in form (e.g. "Sign in with...")
-      name: "Credentials",
+      name: 'Credentials',
       // `credentials` is used to generate a form on the sign in page.
       // You can specify which fields should be submitted, by adding keys to the `credentials` object.
       // e.g. domain, username, password, 2FA token, etc.
       // You can pass any HTML attribute to the <input> tag through the object.
       credentials: {
-        username: { label: "Email", type: "email", placeholder: "email" },
-        password: { label: "Password", type: "password" }
+        username: { label: 'Email', type: 'email', placeholder: 'email' },
+        password: { label: 'Password', type: 'password' },
       },
       async authorize(credentials, req) {
         if (!credentials) return null;
 
-        const user = await userLogIn(credentials.username, credentials.password);
+        const user = await userLogIn(
+          credentials.username,
+          credentials.password
+        );
 
         if (user) {
           // Any object returned will be saved in `user` property of the JWT
-          return user
+          return user;
         } else {
           // If you return null then an error will be displayed advising the user to check their details.
-          return null
+          return null;
 
           // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
         }
-      }
-    })
+      },
+    }),
   ],
-  session: { strategy: "jwt" },
+  session: { strategy: 'jwt' },
   callbacks: {
     async jwt({ token, user }) {
       return { ...token, ...user };
@@ -42,5 +45,5 @@ export const authOptions: AuthOptions = {
       session.user = token as any;
       return session;
     },
-  }
-}
+  },
+};
